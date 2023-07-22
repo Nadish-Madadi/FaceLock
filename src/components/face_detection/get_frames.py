9544980ad09359524544a61b7c0d63b124c6b8a0
Nadish-Madadi <75193860+Nadish-Madadi@users.noreply.github.com>
2023-07-22 15:35:10 -0400

    opencv camera embedding test

---
+++ src/components/face_detection/get_frames.py
@@ -0,0 +1,13 @@
+import cv2
+
+class Camera():
+    def __init__(self):
+        self.video = cv2.VideoCapture(0)
+
+    def __delete__(self):
+        self.video.release()
+    
+    def frame_capture(self):
+        ret, frame = self.video.read()
+        ret, jpeg = cv2.imencode(".jpg", frame)
+        return jpeg.tobytes()
---
+++ src/components/face_detection/index.js
@@ -0,0 +1,11 @@
+import React, {useState} from "react";
+
+const Camera = () => {
+  return (
+    <div>
+      <img src="http://localhost:5000/video_feed" alt="Video Frame" />
+    </div>
+  );
+};
+
+export default Camera;
