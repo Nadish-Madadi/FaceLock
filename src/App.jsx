725b9bd098b4188ca5075dc24d0e70994b997d2a
RickLiu1203 <rickliu1203@gmail.com>
2023-07-22 00:37:11 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <h1 className='flex justify-center items-center h-screen text-2xl'>Hello World</h1>
+  )
+}
+
+export default App
