893c90b14de084ab14d353e2c926172d526eb67f
arcticzombieyt <zszombiesize@gmail.com>
2023-07-22 17:13:30 -0400

    Added camera video viewing login page

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -46,28 +46,7 @@ function Login() {
           </div>
         </div>
       </animated.section>
-      <animated.section
-        style={fadeIn}
-        className="min-h-screen bg-white text-black flex flex-col items-center justify-center space-y-9 py-20"
-      >
-        <h1 className="text-2xl sm:text-4xl font-bold text-center space-y-4">
-          Authenticate using facial recognition
-        </h1>
-        <div className="flex flex-col items-center space-y-14">
-          <img
-            src={personImage}
-            alt="FaceIcon"
-            className="w-48 sm:w-64 lg:w-5/12"
-          />
-          {/* aight rick replace the image above ^. this is where you can do ur magic and do the webcam stuffs ig idk */}
-          <p class="text-sm sm:text-lg lg:text-2xl text-center">
-            Click the button below to scan your face
-          </p>
-          <button className="bg-blue-400 text-white py-2 px-4 rounded hover:text-gray-300 hover:bg-blue-500 w-52">
-            Scan Face
-          </button>
-        </div>
-      </animated.section>
+      
     </main>
   );
 }
