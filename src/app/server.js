aadd0e37b0f04839842ec78eda2be1e48744d8e0
Eric Kang <kang.eric.hi@gmail.com>
2023-07-22 23:31:30 -0400

    login functions

--- src/app/server.js
+++ src/app/server.js
@@ -4,23 +4,46 @@ const saveImages = require("../backend/helpers/saveImages");
 const app = express();
 app.use(express.json());
 
+app.post("/login", (req, res) => {
+    collection.findOne({ username: req.body.username})
+        .then((res) => {
+            bcrypt.compare(req.body.password, hash, function(err, result) {
+                if (result) {
+                    return res.status(200).json({
+                        message: "Success",
+                    })
+                } else {
+                    return res.status(403).json({
+                        message: "Login failed",
+                        _id: returnedId
+                    })
+                }
+            });
+        });
+    
+})
+
 app.post("/upload", (req, res) => {
     const frames = JSON.stringify(req.body.frames);
-    try {
-        const returnedId = saveImages({
-            user: req.body.username,
-            frames
-        });
-        return res.status(200).json({
-            message: "Success",
-            _id: returnedId
-        })
-    } catch (e) {
-        return res.status(400).json({
-            error: "Error saving to database.",
-            stack: e
-        })
-    }
+    bcrypt.hash(req.body.password, 10, function(err, hash) {
+        try {
+            const returnedId = saveImages({
+                username: req.body.username,
+                frames,
+                password: hash
+            });
+            return res.status(200).json({
+                message: "Success",
+                _id: returnedId
+            })
+        } catch (e) {
+            return res.status(400).json({
+                error: "Error saving to database.",
+                stack: e
+            })
+        }
+    });
+    
 });
 
 app.listen(3000, (err) => {
